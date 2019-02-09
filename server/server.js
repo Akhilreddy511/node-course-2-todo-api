6cf1186261eabf73c19df18979e6aad78014e4b3
Akhilreddy511 <akhilreddynarayana511@gmail.com>
2019-02-09 15:04:38 +0530

    getting by id in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var ObjectID = require('mongodb').ObjectID;
 
  var mongoose = require('./db/mongoose');
 var Todo = require('./models/todo');
@@ -32,7 +33,24 @@ app.get('/todos',(req,res)=>{
         res.status(400).send(e);
     })
 })
+app.get('/todos:id',(req,res)=>{
+    console.log(req.params);
+    var id = req.params.id;
 
+    if(ObjectID.isValid(id)){
+        console.log('entered valid id')
+    }else{
+        res.status(404).send();
+    }
+
+     User.findById(id).then((doc)=>{
+         console.log(doc);
+         res.send(doc);
+     },(e)=>{
+         res.send({message:'unable to fetech data'});
+     })
+
+})
 app.listen('3000',()=>{
     console.log('am listing at port number 3000');
 })
